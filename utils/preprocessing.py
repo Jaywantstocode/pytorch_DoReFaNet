48788463d0d9e66dc8a341bc552e7b12498213b8
Jayson <jayson.hao@mail.utoronto.ca>
2023-04-18 15:36:29 -0400

    idk what im doing tbh

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,4 +1,5 @@
 import torchvision.transforms as transforms
+import torchvision.datasets as dset
 
 
 def cifar_transform(is_training=True):
@@ -49,3 +50,30 @@ def coco_transform(is_training=True):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
     return transform_list
+
+
+def voc_transform(is_training=True):
+    if is_training:
+        transform_list = [
+            transforms.Resize((448, 448)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.3),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    else:
+        transform_list = [
+            transforms.Resize((448, 448)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+
+    transform_list = transforms.Compose(transform_list)
+    return transform_list
+
+def create_voc_datasets(data_dir, is_training):
+    transform = voc_transform(is_training)
+    voc_dataset = dset.VOCDetection(
+        root=data_dir, year="2007", image_set="train" if is_training else "val", transform=transform, download=True
+    )
+    return voc_dataset
